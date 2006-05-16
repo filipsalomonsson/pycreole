9906a650ecbd714a3b0a109918bca80ecd49351a
Filip Salomonsson <filip@infix.se>
2006-05-16 16:41:17 +0200

    Added some URL and storage handling.

--- creole.py
+++ creole.py
@@ -8,19 +8,42 @@ import urllib2
 if __name__ == '__main__':
     import sys
     from optparse import OptionParser
+    import urlparse
+    import re
+    import os.path
+    import md5
 
     # Set up and run the option parser
     usage = "usage: %prog [options] URL ..."
     op = OptionParser(usage)
-    op.add_option("-d", "--dir", dest="dir", help="storage directory")
+    op.add_option("-d", "--dir", dest="dir", default=".",
+                  help="storage directory")
     (options, args) = op.parse_args()
 
     # We need at least one URL
     if len(args) < 1:
         op.error("no URL given")
 
-    # Fetch the requested URL and print its contents
-    url = args[0]
+    # Clean up the URL (get rid of any fragment identifier)
+    url_parts = urlparse.urlsplit(args[0], 'http')
+    url = urlparse.urlunsplit(url_parts[:-1] + ('',))
+
+    # Path, including parameters (unique identifier within a host)
+    path = urlparse.urlunsplit(('', '') + url_parts[2:-1] + ('',))
+
+    # Host, without default port
+    host = re.sub(r':80$', '', url_parts[1])
+
+    # Fetch the requested URL
     res = urllib2.urlopen(url)
     data = res.read()
-    print data
+
+    store_dir = os.path.join(options.dir, host)
+    # Create store directory if it doesn't already exist.
+    if not os.access(store_dir, os.F_OK):
+        os.makedirs(store_dir)
+        
+    print url
+    print path
+    print host
+    print os.path.join(store_dir, md5.md5(path).hexdigest())
