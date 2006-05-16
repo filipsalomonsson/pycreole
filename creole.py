0707e949c195666d79fe38248a324cdfefb92a79
Filip Salomonsson <filip@infix.se>
2006-05-16 15:54:35 +0200

    Added option parsing.

--- creole.py
+++ creole.py
@@ -7,8 +7,20 @@ import urllib2
 
 if __name__ == '__main__':
     import sys
+    from optparse import OptionParser
 
-    url = sys.argv[1]
+    # Set up and run the option parser
+    usage = "usage: %prog [options] URL ..."
+    op = OptionParser(usage)
+    op.add_option("-d", "--dir", dest="dir", help="storage directory")
+    (options, args) = op.parse_args()
+
+    # We need at least one URL
+    if len(args) < 1:
+        op.error("no URL given")
+
+    # Fetch the requested URL and print its contents
+    url = args[0]
     res = urllib2.urlopen(url)
     data = res.read()
     print data
